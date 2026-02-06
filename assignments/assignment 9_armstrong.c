c925703f699da0111054800f9d6ce9a4409d37a9
Paarth124421 <paarth240427@gmail.com>
2026-02-06 06:57:36 +0000

    Add Armstrong number program implementation

---
+++ assignments/assignment 9_armstrong.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num, temp, remainder, result = 0;
+    
+    printf("Enter an integer:");
+    scanf("%d", &num);
+
+    temp = num;
+    
+    while (temp != 0) {
+        remainder = temp % 10;
+        result += pow(remainder, 3);
+        temp /= 10;
+    }
+    
+    if (result == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+    
+    return 0;
+}
