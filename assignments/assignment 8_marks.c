fceacad711e06043ae12b6e20ba1aaf0b60ac514
Paarth124421 <paarth240427@gmail.com>
2026-02-06 06:43:27 +0000

    Add program to calculate grades based on marks for 5 subjects

---
+++ assignments/assignment 8_marks.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    float marks[5];
+    float total=0;
+    float percentage;
+    printf("Enter marks for 5 subjects:\n");
+    for(int i=0;i=5;i++){
+        printf("subject: %d",i+1);
+        scanf("%d", &marks[i]);\
+        total+=marks[i];
+    }
+    percentage=total/5;
+    
+    if (percentage>=75){
+        printf("Grade: A\n");
+    }
+    else if (percentage>=60){
+        printf("Grade: B\n");
+    }
+    else if (percentage>=50){
+        printf("Grade: C\n");
+    }
+    else if (percentage>=40){
+        printf("Grade: D\n");
+    }
+    else{
+        printf("Grade: F\n");   
+    }
+}
