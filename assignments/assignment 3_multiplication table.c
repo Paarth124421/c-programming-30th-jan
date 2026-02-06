208563453c30d13e9dc551f0ed550580acd5749c
Paarth124421 <paarth240427@gmail.com>
2026-02-06 06:07:29 +0000

    Refactor code structure for improved readability and maintainability

---
+++ assignments/assignment 3_multiplication table.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num,i;
+    printf("enter a number:");
+    scanf("%d",&num);
+    printf("Multiplication table of %d:\n",num);
+    for(i=1;i<=10;i++){
+        printf("%d x %d = %d\n",num,i,num*i);
+}
