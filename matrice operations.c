9b9eec922cd2881b358389171a32a45ce77d764f
Paarth124421 <paarth240427@gmail.com>
2026-02-26 06:24:21 +0000

    Add matrix operations for addition, saddle point detection, and inversion

--- matrice operations.c
+++ matrice operations.c
@@ -1,39 +1,104 @@
 #include <stdio.h>
 int main(){
     int matrix[3][3];
+    int matrix2[3][3];
     int i, j;
-    int k,l;
+    int choice;
 
-    printf("Enter elements for the 3x3 matrix:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("Element [%d][%d]: ", i, j);
-            scanf("%d", &matrix[i][j]);
+    printf("MAIN MENU\n");
+    printf("1. Add two matrices\n");
+    printf("2. find saddle point of two matrices\n");
+    printf("3. inverse of two matrices\n");
+    printf("4. magic square of two matrices\n");
+    printf("5. Exit\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= 4) {
+        printf("Enter elements for the 3x3 matrix:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("Element [%d][%d]: ", i, j);
+                scanf("%d", &matrix[i][j]);
+            }
         }
+
+        printf("\nThe 3x3 matrix is:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("%d ", matrix[i][j]);
+            }
+            printf("\n");
+        }
+        printf("Enter elements for the 3x3 matrix:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("Element [%d][%d]: ", i, j);
+                scanf("%d", &matrix2[i][j]);
+            }
+        }
+
+        printf("\nThe 3x3 matrix is:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("%d ", matrix2[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    switch (choice) {
+        case 1:
+        printf("Sum of matrices:\n");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                printf("%d ", matrix[i][j] + matrix2[i][j]);
+            }
+            printf("\n");
+        }
+        break;
+
+        case 2:
+        int min=0, max=0;
+        printf("saddle point of matrices:\n");
+        for (i = 0; i < 3; i++) {
+            min=matrix[i][0];
+            for (j = 1; j < 3; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }   
+            max=min;
+            for (j = 0; j < 3; j++) {
+                if (matrix[j][i] > max) {
+                    max = matrix[j][i];
+                    printf("saddle points are:"); 
+                    printf("%d %d ", max, min);
+        break;
+        case 3:
+        printf("inverse of matrices:\n");
+        float determinant = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) -
+                            matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]) +
+                            matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+        if (determinant == 0) {
+            printf("The matrix is singular and cannot be inverted.\n");
+        } else {
+            printf("Inverse of the first matrix:\n");
+            printf("%f %f %f\n", (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1]) / determinant,
+                   (matrix[0][2] * matrix[2][1] - matrix[0][1] * matrix[2][2]) / determinant,
+                   (matrix[0][1] * matrix[1][2] - matrix[0][2] * matrix[1][1]) / determinant);
+            printf("%f %f %f\n", (matrix[1][2] * matrix[2][0] - matrix[1][0] * matrix[2][2]) / determinant,
+                   (matrix[0][0] * matrix[2][2] - matrix[0][2] * matrix[2][0]) / determinant,
+                   (matrix[0][2] * matrix[1][0] - matrix[0][0] * matrix[1   ][2]) / determinant);
+            printf("%f %f %f\n", (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]) / determinant,
+                   (matrix[0][1] * matrix[2][0] - matrix[0][0] * matrix[2][1]) / determinant,
+                   (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]) / determinant);
+        }
+        break;                       
+                    
+
     }
 
-    printf("\nThe 3x3 matrix is:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-    printf("Enter elements for the 3x3 matrix:\n");
-    for (k = 0; k < 3; k++) {
-        for (l = 0; l < 3; l++) {
-            printf("Element [%d][%d]: ", k, l);
-            scanf("%d", &matrix[k][l]);
-        }
-    }
-
-    printf("\nThe 3x3 matrix is:\n");
-    for (k = 0; k < 3; k++) {
-        for (l = 0; l < 3; l++) {
-            printf("%d ", matrix[k][l]);
-        }
-        printf("\n");
-    }
 
 
     return 0;
