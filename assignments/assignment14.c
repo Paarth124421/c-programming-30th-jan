062cbc011887689ced44b4deefafa75281fec3d4
Paarth124421 <paarth240427@gmail.com>
2026-03-27 06:55:04 +0000

    add assignment 14

---
+++ assignments/assignment14.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main(){
+    long long binary;
+    int decimal = 0, base = 1, rem;
+    printf("Enter a binary number: ");
+    scanf("%lld", &binary);
+    while (binary > 0) {
+        rem = binary % 10;
+        decimal = decimal + rem * base;
+        binary = binary / 10;
+        base = base * 2;
+    }
+    printf("Decimal equivalent: %d\n", decimal);
+}
